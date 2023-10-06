e6f959a63e1bc17bdb7f3bb55028cc039dfe2ef2
Elizaveta <lizastepanova93@mail.ru>
2023-10-06 23:11:23 +0700

    Настройка окружения, настройка mongoDB, создана первая модель User

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { getMongoDbConfig } from './config/mongo.config'
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module'
+
+@Module({
+	imports: [
+		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoDbConfig,
+		}),
+		AuthModule,
+		UserModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
+})
+export class AppModule {}
