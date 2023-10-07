687eb1e06c348a05634b5e51dd5f23efe9677731
Elizaveta <lizastepanova93@mail.ru>
2023-10-07 18:55:11 +0700

    Добавлен фкнкционал по добавлению, получению, удалению и обновлению users profile

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
-import { UserModel } from './user.model'
 import { ModelType } from '@typegoose/typegoose/lib/types'
 import { InjectModel } from 'nestjs-typegoose'
+import { genSalt, hash } from 'bcryptjs'
+
+import { UpdateUserDto } from './dto/updateUserDto'
+import { UserModel } from './user.model'
 
 @Injectable()
 export class UserService {
@@ -16,5 +19,57 @@ export class UserService {
 			throw new NotFoundException('User not found')
 		}
 		return user
+	}
+
+	async updateProfile(_id: string, dto: UpdateUserDto) {
+		const user = await this.byId(_id)
+		const isSameUser = await this.UserModel.findOne({ email: dto.email })
+
+		if (isSameUser && String(_id) !== String(isSameUser._id)) {
+			throw new NotFoundException('Email busy')
+		}
+
+		if (dto.password) {
+			const salt = await genSalt(10)
+			user.password = await hash(user.password, salt)
+		}
+
+		user.email = dto.email
+
+		if (dto.isAdmin || dto.isAdmin === false) {
+			user.isAdmin = dto.isAdmin
+		}
+
+		await user.save()
+		return
+	}
+
+	async getCount() {
+		return this.UserModel.find().count().exec()
+	}
+
+	async getAll(searchTerm?: string) {
+		let options = {}
+
+		if (searchTerm) {
+			options = {
+				$or: [
+					{
+						email: new RegExp(searchTerm, 'i'),
+					},
+				],
+			}
+		}
+
+		return this.UserModel.find(options)
+			.select('-password -updatedAt -__v')
+			.sort({
+				createdAt: 'desc',
+			})
+			.exec()
+	}
+
+	async delete(id: string) {
+		return this.UserModel.findByIdAndDelete(id).exec()
 	}
 }
