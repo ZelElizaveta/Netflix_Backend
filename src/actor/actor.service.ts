a6954d9fcf72b7ff6c464f2a712ea2e63b4275ff
Elizaveta <lizastepanova93@mail.ru>
2023-10-10 17:36:22 +0700

    Добавленая агрегация к сервису getAll для модуля актер

--- src/actor/actor.service.ts
+++ src/actor/actor.service.ts
@@ -35,10 +35,22 @@ export class ActorService {
 			}
 		}
 
-		return this.ActorModel.find(options)
-			.select('-updatedAt -__v')
+		return this.ActorModel.aggregate()
+			.match(options)
+			.lookup({
+				from: 'Movie',
+				localField: '_id',
+				foreignField: 'actors',
+				as: 'movies',
+			})
+			.addFields({
+				countMovies: {
+					$size: '$movies',
+				},
+			})
+			.project({ __v: 0, updatedAt: 0, movies: 0 })
 			.sort({
-				createdAt: 'desc',
+				createdAt: -1,
 			})
 			.exec()
 	}
