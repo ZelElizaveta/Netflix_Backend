9c3fca502968e1487b1e6a4c7ef84a94b8a2ad6a
Elizaveta <lizastepanova93@mail.ru>
2023-10-11 14:57:56 +0700

    Добавлен сервис получение коллекций в модуле жанры

--- src/genre/genre.module.ts
+++ src/genre/genre.module.ts
@@ -4,6 +4,7 @@ import { TypegooseModule } from 'nestjs-typegoose'
 import { GenreController } from './genre.controller'
 import { GenreService } from './genre.service'
 import { GenreModel } from './genre.model'
+import { MovieModule } from 'src/movie/movie.module'
 
 @Module({
 	controllers: [GenreController],
@@ -16,6 +17,7 @@ import { GenreModel } from './genre.model'
 				},
 			},
 		]),
+		MovieModule,
 	],
 	providers: [GenreService],
 })
--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -4,11 +4,14 @@ import { ModelType } from '@typegoose/typegoose/lib/types'
 
 import { GenreModel } from './genre.model'
 import { CreateGenreDto } from './dto/create-genre.dto'
+import { MovieService } from 'src/movie/movie.service'
+import { Collection } from './genre.interface'
 
 @Injectable()
 export class GenreService {
 	constructor(
-		@InjectModel(GenreModel) private readonly GenreModel: ModelType<GenreModel>
+		@InjectModel(GenreModel) private readonly GenreModel: ModelType<GenreModel>,
+		private readonly MovieService: MovieService
 	) {}
 
 	async bySlug(slug: string) {
@@ -51,9 +54,28 @@ export class GenreService {
 		return this.GenreModel.find().count().exec()
 	}
 
-	async getCollections() {
+	async getCollections(): Promise<Collection[]> {
 		const genres = await this.getAll()
-		const collections = genres
+
+		const collections = await Promise.all(
+			genres.map(async (genre) => {
+				const moviesByGenre = await this.MovieService.byGenres([genre._id])
+				let image = 'default_image.jpg'
+
+				if (moviesByGenre.length > 0) {
+					image = moviesByGenre[0].bigPoster
+				}
+
+				const result: Collection = await {
+					_id: String(genre._id),
+					title: genre.name,
+					slug: genre.slug,
+					image,
+				}
+				return result
+			})
+		)
+
 		return collections
 	}
 
